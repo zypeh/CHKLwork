3fedfb7fc194c9224821d17b4ffa742fcb282b0a
Zypeh <Zypeh.geek@gmail.com>
2016-01-27 22:19:40 +0800

    Added log support, routes added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var koa = require('koa');
-var app = koa();
+var http = require('http'),
+    logger = require('koa-logger'),
+    serve = require('koa-static'),
+    route = require('./routes/routes'),
+    koa = require('koa'),
+    app = koa();
 
-app.use(function *() {
-  this.body = "Hello World!!!";
-});
+app.use(serve('./public'));
 
-app.listen(3000);
+// Route middleware
+app.use(route.get('/', routes.home));
+
+// HTTP Server
+http.createServer(app.Callback()).listen(3000);
+console.log('CHKL|work is working as smooth as silk !');
+console.log(' Server listening on port 3000');
