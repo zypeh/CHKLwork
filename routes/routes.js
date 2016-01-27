48c4c0497d85790a6d80dfaa1b9d35ebd34bd209
Zypeh <Zypeh.geek@gmail.com>
2016-01-27 23:54:43 +0800

    debug, remove monk module

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
 var parse = require('co-body'),
-    render = require('../lib/views'),
-    pages = require('../models/pages')
+    render = require('../lib/views')
+//    pages = require('../models/pages')
 ;
    
 /// Route definitions
 
 // index
 exports.home = function *() {
-	this.body = yield render('hello');
+	this.body = yield render('index');
 }
 
